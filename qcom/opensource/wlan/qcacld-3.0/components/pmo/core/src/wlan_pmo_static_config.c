e62623fd6a556dc56efa7802d3096de276185ebe
eric.luo <eric.luo@nothing.tech>
2026-03-20 23:42:26 +0800

    qcacld-3.0: Remove beacon bit in WOW set by default
    Change-Id: I27459ee9bd9c855a26830748494ae5dcd8007368

--- qcom/opensource/wlan/qcacld-3.0/components/pmo/core/src/wlan_pmo_static_config.c
+++ qcom/opensource/wlan/qcacld-3.0/components/pmo/core/src/wlan_pmo_static_config.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -51,9 +51,6 @@ void pmo_register_wow_wakeup_events(struct wlan_objmgr_vdev *vdev)
 	switch (vdev_opmode) {
 	case QDF_STA_MODE:
 	case QDF_P2P_CLIENT_MODE:
-		pmo_set_wow_event_bitmap(WOW_BEACON_EVENT,
-					 PMO_WOW_MAX_EVENT_BM_LEN,
-					 event_bitmap);
 		/* set power on failure event only for STA and P2P_CLI mode*/
 		psoc_ctx =  pmo_vdev_get_psoc_priv(vdev);
 		if (psoc_ctx->psoc_cfg.auto_power_save_fail_mode ==
